fbf250ec3878bdbdf59c8c16f30dda598858b228
skyy4 <skyonly4code@gmail.com>
2026-01-12 16:46:57 +0530

    [Shashi] Add . Solved Scenario Based Problem

---
+++ dsa-practice/scenario-based/SmartShelf.java
@@ -0,0 +1,23 @@
+package scenario_based;
+
+public class SmartShelf {
+
+    static void sort(String[] a) {
+        for (int i = 1; i < a.length; i++) {
+            String key = a[i];
+            int j = i - 1;
+            while (j >= 0 && a[j].compareToIgnoreCase(key) > 0) {
+                a[j + 1] = a[j];
+                j--;
+            }
+            a[j + 1] = key;
+        }
+    }
+
+    public static void main(String[] args) {
+        String[] books = { "Java", "Algorithms", "Clean Code", "Data" };
+        sort(books);
+        for (String b : books)
+            System.out.println(b);
+    }
+}
