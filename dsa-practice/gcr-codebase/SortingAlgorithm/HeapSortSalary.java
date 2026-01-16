5c1c438a9e393f8f4704ceaf31f543d54912fd09
skyy4 <skyonly4code@gmail.com>
2026-01-16 12:18:12 +0530

    [Shashi] Add. Solved Sorting Algo Problems

---
+++ dsa-practice/gcr-codebase/SortingAlgorithm/HeapSortSalary.java
@@ -0,0 +1,41 @@
+public class HeapSortSalary {
+
+    static void heapify(int[] a, int n, int i) {
+        int largest = i;
+        int l = 2 * i + 1;
+        int r = 2 * i + 2;
+
+        if (l < n && a[l] > a[largest])
+            largest = l;
+        if (r < n && a[r] > a[largest])
+            largest = r;
+
+        if (largest != i) {
+            int t = a[i];
+            a[i] = a[largest];
+            a[largest] = t;
+            heapify(a, n, largest);
+        }
+    }
+
+    static void sort(int[] a) {
+        int n = a.length;
+
+        for (int i = n / 2 - 1; i >= 0; i--)
+            heapify(a, n, i);
+
+        for (int i = n - 1; i > 0; i--) {
+            int t = a[0];
+            a[0] = a[i];
+            a[i] = t;
+            heapify(a, i, 0);
+        }
+    }
+
+    public static void main(String[] args) {
+        int[] sal = { 50000, 30000, 70000, 40000 };
+        sort(sal);
+        for (int x : sal)
+            System.out.print(x + " ");
+    }
+}
