5c055cb785ce6014aaf3d288b51d4807fdd54ef7
skyy4 <skyonly4code@gmail.com>
2026-01-19 16:09:11 +0530

    [Shashi] Add. DAO Layer Of AddressBook

---
+++ oops-practice/AddressBook/AddressBookDAO.java
@@ -0,0 +1,47 @@
+
+import java.util.*;
+
+// DAO LAYER
+// UC4: Store multiple contacts using ArrayList
+// UC5: Store multiple address books using HashMap
+public class AddressBookDAO {
+
+    // UC5: AddressBookName → List of Contacts
+    HashMap<String, ArrayList<Contact>> bookMap = new HashMap<>();
+
+    public void addBook(String name) {
+        bookMap.putIfAbsent(name, new ArrayList<>());
+    }
+
+    public ArrayList<Contact> getBook(String name) {
+        return bookMap.get(name);
+    }
+
+    // UC1 & UC6: Add contact with duplicate check
+    public boolean addContact(String book, Contact c) {
+        ArrayList<Contact> list = bookMap.get(book);
+        if (list.contains(c))
+            return false;
+        list.add(c);
+        return true;
+    }
+
+    // UC2: Find contact for edit
+    public Contact findContact(String book, String fname) {
+        for (Contact c : bookMap.get(book)) {
+            if (c.getFname().equalsIgnoreCase(fname))
+                return c;
+        }
+        return null;
+    }
+
+    // UC3: Delete contact by name
+    public boolean deleteContact(String book, String fname) {
+        ArrayList<Contact> list = bookMap.get(book);
+        return list.removeIf(c -> c.getFname().equalsIgnoreCase(fname));
+    }
+
+    public HashMap<String, ArrayList<Contact>> getAll() {
+        return bookMap;
+    }
+}
